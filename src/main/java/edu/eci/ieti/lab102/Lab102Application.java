c7220cf34f77d1ba17901325bff546ec0018955b
Nikolas2001-13 <nikolas.bernal@mail.escuelaing.edu.co>
2021-08-25 17:29:20 -0500

    First commit

---
+++ src/main/java/edu/eci/ieti/lab102/Lab102Application.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.lab102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab102Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/ieti/lab102/Lab102ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.lab102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
